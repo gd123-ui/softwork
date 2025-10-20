0902fa86ca43ac7a5e9a23e40863b7e5092aa3c0
gd123-ui <15166108510@163.com>
2025-10-20 20:32:05 +0800

    Add files via upload

---
+++ code/apps/coures/adminx.py
@@ -0,0 +1,55 @@
+# encoding: utf-8
+
+import xadmin
+
+from .models import Course, Lesson, Video, CourseResource
+
+
+# Course的admin管理器
+class CourseAdmin(object):
+    list_display = [
+        'name',
+        'desc',
+        'detail',
+        'degree',
+        'learn_times',
+        'students']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students']
+    list_filter = [
+        'name',
+        'desc',
+        'detail',
+        'degree',
+        'learn_times',
+        'students']
+    # 富文本
+    style_fields = {"detail": "ueditor"}
+
+
+# 章节管理
+class LessonAdmin(object):
+    list_display = ['course', 'name', 'add_time']
+    search_fields = ['course', 'name']
+    # __name代表使用外键中name字段
+    list_filter = ['course__name', 'name', 'add_time']
+
+
+# 视频管理
+class VideoAdmin(object):
+    list_display = ['lesson', 'name', 'add_time']
+    search_fields = ['lesson', 'name']
+    list_filter = ['lesson', 'name', 'add_time']
+
+
+# 课程资源管理
+class CourseResourceAdmin(object):
+    list_display = ['course', 'name', 'download', 'add_time']
+    search_fields = ['course', 'name', 'download']
+    list_filter = ['course__name', 'name', 'download', 'add_time']
+
+
+# 将管理器与model进行注册关联
+xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(Lesson, LessonAdmin)
+xadmin.site.register(Video, VideoAdmin)
+xadmin.site.register(CourseResource, CourseResourceAdmin)
---
+++ code/apps/coures/urls.py
@@ -0,0 +1,20 @@
+# encoding: utf-8
+from courses.views import CourseListView, CourseDetailView, CourseInfoView, CommentsView, AddCommentsView, VideoPlayView
+from django.urls import path, re_path
+
+app_name = "courses"
+# 课程管理子路由
+urlpatterns = [
+    # 课程列表url
+    path('list/', CourseListView.as_view(), name="list"),
+    # 课程详情页
+    re_path('detail/(?P<course_id>\d+)/', CourseDetailView.as_view(), name="course_detail"),
+    # 课程章节信息页
+    re_path('info/(?P<course_id>\d+)/', CourseInfoView.as_view(), name="course_info"),
+    # 课程章节信息页
+    re_path('comments/(?P<course_id>\d+)/', CommentsView.as_view(), name="course_comments"),
+    # 添加课程评论
+    path('add_comment/', AddCommentsView.as_view(), name="add_comment"),
+    # 课程视频播放页
+    re_path('video/(?P<video_id>\d+)/', VideoPlayView.as_view(), name="video_play"),
+]
